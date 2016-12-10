4f390784d00fff505649c4eefa614c24306b5154
Anda22Stefania <anda_AMS@yahoo.com>
2016-12-10 19:03:40 +0200

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,8 @@
-﻿using Android.App;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using System.Collections.Generic;
 
 namespace Organizator2
 {
@@ -9,27 +10,30 @@ namespace Organizator2
     public class MainActivity : Activity
     {
         Button Home, Events, Grades;
+        
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            Home = FindViewById<Button>(Resource.Id.btnHome);
-            Events = FindViewById<Button>(Resource.Id.btnEvents);
-            Grades = FindViewById<Button>(Resource.Id.btnGrades);
+           
 
-            Events.Click += Events_Click;
+              Home = FindViewById<Button>(Resource.Id.btnHome);
+              Events = FindViewById<Button>(Resource.Id.btnEvents);
+              Grades = FindViewById<Button>(Resource.Id.btnGrades);
+            
 
 
+            Events.Click += (object sender, System.EventArgs e)=>
 
-        }
-
-        private void Events_Click(object sender, System.EventArgs e)
         {
-            var intent = new Intent(this, typeof(Events));
-            StartActivity(intent);
+           
+            StartActivity(typeof(Events));
+        };
+
         }
     }
-    }
+}
 
 
