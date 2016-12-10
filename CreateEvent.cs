0fd00aee51d6487cb43a9be724adf29c6dee2de2
Anda22Stefania <anda_AMS@yahoo.com>
2016-12-10 19:05:41 +0200

    Update CreateEvent.cs

--- CreateEvent.cs
+++ CreateEvent.cs
@@ -11,27 +11,69 @@ using Android.Views;
 using Android.Widget;
 
 namespace Organizator2
-{
-    [Activity(Label = "Activity1")]
+{   //AddEvent handler
+    public class OnCreateEvent : EventArgs
+    {
+
+        private string title, description;
+        
+        public string EvTitle
+        {
+            get { return title; }
+            set { title = value; }
+
+        }
+
+        public string EvDescription
+        {
+
+            get { return description; }
+            set { description = value; }
+           
+        }
+        public OnCreateEvent(string title, string descritpion) : base()
+        {
+            EvTitle = title;
+            EvDescription = description;
+        }
+    }
+
+
+    [Activity(Label = "CreateEvent")]
     public class CreateEvent : Activity
     {
         Button createEvent, Back;
+        EditText EventTitle, EventDescription;
+        public event EventHandler<OnCreateEvent> EventCreation;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.createEvent);
+
+
             createEvent = FindViewById<Button>(Resource.Id.btnCreateEvent);
             Back = FindViewById<Button>(Resource.Id.btnBack);
+            EventTitle = FindViewById<EditText>(Resource.Id.txtTitle);
+            EventDescription = FindViewById<EditText>(Resource.Id.txtDescription);
+
 
             Back.Click += Back_Click;
+            createEvent.Click += CreateEvent_Click;
+           
+        }
 
-
+        private void CreateEvent_Click(object sender, EventArgs e)
+        {
+            if (EventCreation != null)
+            {
+                EventCreation.Invoke(this, new OnCreateEvent(EventTitle.Text, EventDescription.Text));
+            }
         }
 
         private void Back_Click(object sender, EventArgs e)
         {
-            var intent = new Intent(this, typeof(Events));
-            StartActivity(intent);
+            
+            StartActivity(typeof(Events));
 
         }
     }
